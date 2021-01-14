e6e80c9ede5436a4fbc8554262656cd71224197a
TJ <abongsjoel@gmail.com>
2021-01-14 17:07:21 +0100

    Tests structure

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,38 @@
+describe('Our first suite', () => {
+    describe ('Our suite section', () => {
+        beforeEach('code for every test', () => {
+            //repetitive code
+        })
+        it('some test code', () => {
+
+        })
+        it('some test code', () => {
+
+        })
+        it('some test code', () => {
+
+        })
+    })
+
+    it('first test', () => {
+        
+    })
+    it('second test', () => {
+
+    })
+    it('third test', () => {
+
+    })
+})
+
+describe('Our Second suite', () => {
+    it('first test', () => {
+        
+    })
+    it('second test', () => {
+
+    })
+    it('third test', () => {
+
+    })
+})
