7cf1475221e23d56a8b191c66502cf7ec4bb294c
TJ <abongsjoel@gmail.com>
2021-01-23 02:04:45 +0100

    Verify a single drop down element

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -179,6 +179,8 @@ describe('Our first suite', () => {
 
     cy.get('nav nb-select').click()
     cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
   })
 })
 
