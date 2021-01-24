9f5420958782be1392f2c5586eb15b2fbba137f1
TJ <abongsjoel@gmail.com>
2021-01-25 00:26:20 +0100

    Popups and Tooltips

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -272,7 +272,7 @@ describe('Our first suite', () => {
 
   })
 
-  it.only('tooltip', () => {
+  it('tooltip', () => {
     cy.visit('/')
     cy.contains('Modal & Overlays').click()
     cy.contains('Tooltip').click()
@@ -280,6 +280,31 @@ describe('Our first suite', () => {
     cy.contains('nb-card', 'Colored Tooltips')
       .contains('Default').click()
     cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+
+  })
+
+  it.only('dialog box', () => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    // 1
+    // cy.get('tbody tr').first().find('.nb-trash').click()
+    // cy.on('window:confirm', confirm => {
+    //   expect(confirm).to.equal('Are you sure you want to delete?')
+    // })
+
+    //2
+    // const stub = cy.stub()
+    // cy.on('window:confirm', stub)
+    // cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+    //   expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+    // })
+
+    //3
+    cy.get('tbody tr').first().find('.nb-trash').click()
+    cy.on('window:confirm', () => false)
+
 
   })
 })
